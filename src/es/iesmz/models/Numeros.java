7825e2ada2fc114678aadbe25fd866ea6e7a61ca
Iris <irisaunav@gmail.com>
2026-05-27 21:09:45 +0200

    Anadir clase de Numeros

---
+++ src/es/iesmz/models/Numeros.java
@@ -0,0 +1,13 @@
+package es.iesmz.models;
+
+public class Numeros {
+    public static int sonIguales(int a, int b, int c) {
+        if (b == c && a == c) {
+            return 0;
+        } else if (a == b || a == c || b == c) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
